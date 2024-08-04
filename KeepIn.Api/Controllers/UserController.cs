8abba52358ec135292eac4738cda4da276cb96d8
rorychatt <pavel.rinne@gmail.com>
2024-08-04 16:48:36 +0200

    Feat: controller setup

--- KeepIn.Api/Controllers/UserController.cs
+++ KeepIn.Api/Controllers/UserController.cs
@@ -14,4 +14,25 @@ public class UserController(IUserRepository userRepository) : ControllerBase
         var users = userRepository.GetUsersAsync();
         return Ok(users);
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<User?> GetUserById(string id)
+    {
+        var user = userRepository.GetUserByIdAsync(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public ActionResult<User?> CreateUser(User user)
+    {
+        var createdUser = userRepository.CreateUserAsync(user);
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, createdUser);
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteUser(string id)
+    {
+        userRepository.DeleteUserAsync(id);
+        return Ok();
+    }
 }
