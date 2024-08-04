eceff6c7555f03b77e0807d7b9a3431163efdb93
rorychatt <pavel.rinne@gmail.com>
2024-08-04 17:22:14 +0200

    Feat: better endpoints and integration tests

--- KeepIn.Api/Controllers/UsersController.cs
+++ KeepIn.Api/Controllers/UsersController.cs
@@ -5,34 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 namespace KeepIn.Api.Controllers;
 
 [ApiController]
-[Route("api/v1/Inventory")]
-public class UserController(IUserRepository userRepository) : ControllerBase
+[Route("api/v1/Users")]
+public class UsersController(IUsersRepository usersRepository) : ControllerBase
 {
     [HttpGet]
     public ActionResult<IEnumerable<User>> GetUsers()
     {
-        var users = userRepository.GetUsersAsync();
+        var users = usersRepository.GetUsersAsync();
         return Ok(users);
     }
 
     [HttpGet("{id}")]
     public ActionResult<User?> GetUserById(string id)
     {
-        var user = userRepository.GetUserByIdAsync(id);
+        var user = usersRepository.GetUserByIdAsync(id);
         return Ok(user);
     }
 
     [HttpPost]
     public ActionResult<User?> CreateUser(User user)
     {
-        var createdUser = userRepository.CreateUserAsync(user);
+        var createdUser = usersRepository.CreateUserAsync(user);
         return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, createdUser);
     }
 
     [HttpDelete("{id}")]
     public ActionResult DeleteUser(string id)
     {
-        userRepository.DeleteUserAsync(id);
+        usersRepository.DeleteUserAsync(id);
         return Ok();
     }
 }
---
+++ KeepIn.Tests/ControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+namespace KeepIn.Tests
+{
+    public class ControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client = factory.CreateClient();
+
+        [Fact]
+        public async Task GetUsers_ShouldReturnOk()
+        {
+            var response = await _client.GetAsync("/api/v1/Users");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.NotNull(responseString);
+        }
+    }
+}
