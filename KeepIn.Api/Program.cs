365ed53b5575e94a0d90aa2ce31cec45e59e2947
rorychatt <pavel.rinne@gmail.com>
2024-08-04 17:21:51 +0200

    Feat: import partial class for testing

--- KeepIn.Api/Program.cs
+++ KeepIn.Api/Program.cs
@@ -1,9 +1,12 @@
+using KeepIn.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<IUsersRepository, UsersRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,3 +26,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program
+{
+}
